f9312c21fef96dff43c77d286a53f97f3d5736a6
Patitobi <kesseltobias0@gmail.com>
2022-02-21 20:06:09 +0100

    Create PyBot.py

---
+++ PyBot.py
@@ -0,0 +1,34 @@
+import discord
+
+import asyncio
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Hallo {}'.format(client.user.name))
+    client.loop.create_task(status_task())
+
+
+async def status_task():
+    while True:
+        await client.change_presence(activity=discord.Game('Max'), status=discord.Status.online)
+        await asyncio.sleep(3)
+        await client.change_presence(activity=discord.Game('ist'), status=discord.Status.online)
+        await asyncio.sleep(3)
+        await client.change_presence(activity=discord.Game('ein'), status=discord.Status.online)
+        await asyncio.sleep(3)
+        await client.change_presence(activity=discord.Game('Diker Sack'), status=discord.Status.online)
+        await asyncio.sleep(3)
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if '-help' in message.content:
+        await message.channel.send('**Command`s die du verwenden kannst:**\r\n'
+                                    '-help, -userinfo, -github')
+
+client.run('OTQ1MzQ2ODYwMDc0MjgzMDE4.YhO03w.mpq7j_ths58fWjvxw95ompIvYlY')
